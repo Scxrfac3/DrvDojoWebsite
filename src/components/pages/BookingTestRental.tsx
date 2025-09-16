2709689108c37b629b6be42a66517c798cb5b7ad
Scxrfac3 <45169025+Scxrfac3@users.noreply.github.com>
2025-09-16 14:01:04 +0100

    dcsdc

--- src/components/pages/BookingTestRental.tsx
+++ src/components/pages/BookingTestRental.tsx
@@ -21,7 +21,7 @@ const BookingTestRental = () => {
 
   const calendlyEmbedHTML = `
     <!-- Calendly inline widget begin -->
-<div class="calendly-inline-widget" data-url="https://calendly.com/drivedojo-qnua/mock-driving-test-clone" style="min-width:320px;height:700px;"></div>
+<div class="calendly-inline-widget" data-url="https://calendly.com/drivedojo-qnua/testcarrental" style="min-width:320px;height:700px;"></div>
 <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
 <!-- Calendly inline widget end -->
   `;
